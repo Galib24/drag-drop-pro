74c0641d2e6f86bfd996259f21d85dd94f139472
SQLAdmin <abuyeahia24@gmail.com>
2023-11-05 02:04:00 +0600

    added drag and drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-
+import { AiOutlinePicture } from 'react-icons/ai';
 import './App.css';
 
 function App() {
@@ -7,10 +7,40 @@ function App() {
   const [selectedImages, setSelectedImages] = useState([]);
   const [savedImagesCount, setSavedImagesCount] = useState(0);
   const [selectedForDeletionCount, setSelectedForDeletionCount] = useState(0);
+  const [selectedCount, setSelectedCount] = useState(0); // Initialize the counter
 
+  // drag and drop part
+  const [draggedImage, setDraggedImage] = useState(null);
 
+  const handleDragStart = (e, picture) => {
+    setDraggedImage(picture);
+    e.dataTransfer.setData('text/plain', ''); // Required for Firefox to enable drag-and-drop
+  };
 
+  const handleDragEnter = (e, picture) => {
+    e.preventDefault();
+  };
 
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e, picture) => {
+    e.preventDefault();
+
+    if (draggedImage !== picture) {
+      const updatedDates = [...dates];
+      const draggedIndex = updatedDates.indexOf(draggedImage);
+      const dropIndex = updatedDates.indexOf(picture);
+
+      updatedDates[draggedIndex] = picture;
+      updatedDates[dropIndex] = draggedImage;
+
+      setDates(updatedDates);
+    }
+
+    setDraggedImage(null);
+  };
 
   useEffect(() => {
     // When the component mounts, retrieve the selected images from local storage
@@ -86,26 +116,75 @@ function App() {
 
   };
 
+  const handleImageSelection = (picture) => {
+    // Toggle the selected state of the image
+    picture.selected = !picture.selected;
+    setDates([...dates]);
+
+    // Update the counter
+    setSelectedCount((prevCount) => (picture.selected ? (prevCount + 1) : prevCount === null ? null : prevCount - 1));
+  };
+
+
 
 
   return (
-    <section className='container  mt-8 ml-16 pr-48 pl-48   bg-gray-800'>
+    <section className='container   ml-16 pr-52 pl-52 shadow-lg rounded-xl'>
 
       {/* <p>Number of saved images: {savedImagesCount}</p> */}
-      <div className='bg-red-500 w-auto h-10'> nav</div>
-      {selectedImages.some((image) => image.selected) && (
-        <button className='btn bg-red-400' onClick={deleteSelectedImages}>Delete Selected Images</button>
-      )}
-      {dates.some((picture) => picture.selected) && (
-        <button className='btn bg-red-400' onClick={selectAndDeleteImagesFromDates}>
-          Delete Selected Images from Dates
-        </button>
-      )}
 
-      {/* <p>Number of images selected for deletion: {selectedForDeletionCount}</p> */}
-      <div className="images grid grid-cols-4 gap-2">
+      {/* nav bar */}
+      <div style={{ border: '1px solid black' }}
+        className='bg-white w-auto h-10 navbar overflow-clip '>
+        <div className='navbar-start'>
+          <input type="checkbox" className=" checkbox checkbox-sm mt-2 checkbox-info " disabled checked />
+          {selectedCount !== null && selectedCount > 0 && <p>{selectedCount} Files Selected </p>}
+
+
+          {selectedForDeletionCount && <p>Files Selected  </p>}
+
+        </div>
+
+
+        <div className='navbar-end overflow-clip'>
+          {selectedImages.some((image) => image.selected) && (
+            <button className='btn w-fit bg-white text-red-500 hover:bg-red-800 hover:text-white' onClick={deleteSelectedImages}>
+              Delete Files</button>
+          )}
+          {dates.some((picture) => picture.selected) && (
+            <button className='btn  w-fit bg-white text-red-500 hover:bg-red-800 hover:text-white' onClick={selectAndDeleteImagesFromDates}>
+              Delete Files
+            </button>
+          )}
+        </div>
+      </div>
+
+
+
+
+      {/* <p>Selected for deletion: </p> */}
+      {/* <p>Number of images selected for deletion: </p> */}
+      <div className="images grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-3">
         {selectedImages.map((image, index) => (
-          <div key={index} className="relative group" >
+          <div key={index}
+            className={`relative rounded-md group cursor-pointer `}
+            style={{
+              marginTop: '10px',
+              width: '150px',
+              height: '150px',
+              transition: 'transform 0.3s, box-shadow 0.3s', // Add transitions for smooth effects
+              transform: `scale(${draggedImage === image ? 1.1 : 1})`, // Apply scale transformation
+              boxShadow: draggedImage === image ? '0px 10px 20px rgba(0, 0, 0, 0.2)' : '0px 0px 0px rgba(0, 0, 0, 0)', // Apply drop shadow
+           
+
+            }}
+            draggable
+            onDragStart={(e) => handleDragStart(e, picture)}
+            onDragEnter={(e) => handleDragEnter(e, picture)}
+            onDragOver={(e) => handleDragOver(e)}
+            onDrop={(e) => handleDrop(e, picture)}
+          >
+            <div className="absolute rounded-md inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity"></div>
             <img
               src={image.url}
               alt=""
@@ -123,17 +202,29 @@ function App() {
         {dates?.map((picture, index) => (
           <div
             key={picture.id}
-            className={`relative group cursor-pointer ${picture.selected ? 'bg-white' : 'bg-white'}`}
-            style={{ width: '150px', height: '150px',
-              ...(index === 0 ? { width: '280px', height: '280px', marginRight: '20px' } : {}), 
-              
-           
-            }
-            }
-            
-            
+
+            className={`relative rounded-md group cursor-pointer ${picture.selected ? 'bg-white' : 'bg-white'} `}
+            style={{
+              marginTop: '10px',
+              width: '150px',
+              height: '150px',
+              transition: 'transform 0.3s, box-shadow 0.3s', // Add transitions for smooth effects
+              transform: `scale(${draggedImage === picture ? 1.1 : 1})`, // Apply scale transformation
+              boxShadow: draggedImage === picture ? '0px 10px 20px rgba(0, 0, 0, 0.2)' : '0px 0px 0px rgba(0, 0, 0, 0)', // Apply drop shadow
+              ...(index === 0 ? { width: '280px', height: '280px', marginRight: '20px' } : {}),
+              ...(index === 1 ? { marginLeft: '100px' } : {}),
+              ...(index === 2 ? { marginLeft: '70px' } : {}),
+              ...(index === 3 ? { marginLeft: '40px' } : {}),
+
+            }}
+            draggable
+            onDragStart={(e) => handleDragStart(e, picture)}
+            onDragEnter={(e) => handleDragEnter(e, picture)}
+            onDragOver={(e) => handleDragOver(e)}
+            onDrop={(e) => handleDrop(e, picture)}
+
           >
-            <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity"></div>
+            <div className="absolute rounded-md inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity"></div>
             <img
               src={picture.url}
               alt=""
@@ -142,20 +233,22 @@ function App() {
             <input
               type="checkbox"
               checked={picture.selected}
-              onChange={() => {
-                // Toggle the selected state of the image
-                picture.selected = !picture.selected;
-                setDates([...dates]);
-              }}
+              onChange={() => handleImageSelection(picture)}
               className="absolute top-2 right-2 z-10 cursor-pointer"
             />
           </div>
         ))}
 
-        <label style={{ width: '150px', height: '150px' }} className="custom-file-input">
-          *Add Images
+        <label style={{ width: '150px', height: '150px' }} className="custom-file-input rounded-md">
+          <span>
+
+            <center>
+              <AiOutlinePicture></AiOutlinePicture>
+            </center>
+          </span>
+          Add Images
           <br />
-          <span>Up to 10 images</span>
+
           <input
             type="file"
             name="images"
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='max-w-screen-xl mx-auto'>
     <App />
+    </div>
   </React.StrictMode>,
 )
